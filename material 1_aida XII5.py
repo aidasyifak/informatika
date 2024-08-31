7218c6082cfb86be50bdb720baa29364ac5c6ae2
aidasyifak <145946827+aidasyifak@users.noreply.github.com>
2024-08-31 11:24:59 +0700

    Add files via upload

---
+++ material 1_aida XII5.py
@@ -0,0 +1,23 @@
+print("Hello World")
+print("Belajar Python dari Nol")
+judul = "Belajar Pemrograman Python sampai Bisa"
+print(judul)
+## Snake Case digunakan pada: module_name, package_name, method_name, function_name , global_var_name,instance_var_name, function_parameter_name, local_var_name
+## Camel Case digunakan Pada: ClassName, ExceptionName
+## Kebab Case digunakan pada: btn-primary
+## ALL CAPS digunakan Pada: GLOBAL_CONSTANT_NAME
+
+username = 'pythonpemula'
+if username == 'pythonpemula':
+   print("Selamat Datang Admin")
+   print("Silakan ambil tempat duduk")
+
+variabel_ku = "ini isi variabel"
+variabel2 = 20
+print(variabel_ku)
+print(variabel2)
+
+    nama = "python"
+    print nama
+    del(nama)
+    print nama
